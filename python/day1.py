b934da3d6b083a22abe290c3d0201b07f6e701bc
Frans Hoekstra <fransh@schoolmaster.nl>
2020-12-01 08:10:02 +0100

    day1: less is more

--- python/day1.py
+++ python/day1.py
@@ -1,11 +1,7 @@
-answerA,answerB=0,0
-f = open("../input/day1.txt", "r")
-lines=[int(line) for line in f]
-f.close()
+lines=[int (line) for line in open("../input/day1.txt","r")]
 for x in lines:
     for y in lines:
-        if x + y == 2020: answerA = x * y        
+        if x + y == 2020: answerA = x * y
         for z in lines:
             if x + y + z == 2020: answerB = x * y * z
-print ("AnswerA: " , answerA)
-print ("AnswerB: " , answerB)
+print (answerA, answerB)
