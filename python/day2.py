4e57ec8a0d9b13626ae69f4b14fca911f1eec3c5
Frans Hoekstra <fransh@schoolmaster.nl>
2020-12-02 08:17:46 +0100

    xor

--- python/day2.py
+++ python/day2.py
@@ -5,6 +5,6 @@ for line in f:
     min,max=tuple(int(e) for e in t[0].split('-'))
     c=t[1][0]
     if t[2].count(c)>=min and t[2].count(c)<=max: answerA+=1
-    if (t[2][min-1] == c and t[2][max-1] != c) or (t[2][max-1] == c and t[2][min-1] != c) : answerB+=1
+    if (t[2][min-1] == c) != (t[2][max-1] == c) : answerB+=1
 f.close()
 print(answerA, answerB)
