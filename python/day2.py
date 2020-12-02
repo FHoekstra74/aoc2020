1f37e497d7ecc6ec1f8ebe68e340ee71b28a1d06
Frans Hoekstra <fransh@schoolmaster.nl>
2020-12-02 11:01:50 +0100

    less is more and still persist to not to use regex, aka split-to-the-max

--- python/day2.py
+++ python/day2.py
@@ -1,10 +1,2 @@
-f = open("../input/day2.txt","r")
-answerA,answerB=0,0
-for line in f:
-    t=line.split()
-    min,max=tuple(int(e) for e in t[0].split('-'))
-    c=t[1][0]
-    if t[2].count(c)>=min and t[2].count(c)<=max: answerA+=1
-    if (t[2][min-1] == c) != (t[2][max-1] == c) : answerB+=1
-f.close()
-print(answerA, answerB)
+items=[list(int(e) for e in line.split()[0].split('-')) + [(line.split()[1][0])] + [(line.split()[2])]for line in open("../input/day2.txt","r")]
+print(f"a:{len([pwd for min,max,c,pwd in items if pwd.count(c)>=min and pwd.count(c)<=max])} b:{len([pwd for min,max,c,pwd in items if (pwd[min-1]==c) != (pwd[max-1]==c)])}")
