da75fbfcc8bf5978f11afb59443de835a5a7d00e
frans <frans.hoekstra@gmail.com>
2020-12-04 19:57:11 +0100

    day4: less is more

--- python/day4.py
+++ python/day4.py
@@ -1,34 +1,24 @@
 import re
-def allrequired(passport):
-    return {'byr','iyr','eyr','hgt','hcl','ecl','pid'} <= passport.keys()
-def validyear(year,min,max):
-    return int(year)>=min and int(year)<=max
 def validheight(height):
-    res=False
-    if height[-2:]=='cm':
-        res=height[:-2].isnumeric() and int(height[:-2]) >= 150 and int(height[:-2]) <= 193
-    if height[-2:]=='in':
-        res=height[:-2].isnumeric() and int(height[:-2]) >= 59 and int(height[:-2]) <= 76
-    return res
+    if height[-2:]=='cm': return height[:-2].isnumeric() and int(height[:-2]) >= 150 and int(height[:-2]) <= 193
+    elif height[-2:]=='in': return height[:-2].isnumeric() and int(height[:-2]) >= 59 and int(height[:-2]) <= 76
+    else: return False
 
-answera,answerb,lst,passport,f=0,0,[],{},open("../input/day4.txt", "r")
-for l in f:
-    if len(l.strip()) == 0:
-        if len(passport)>0:
-            lst.append(passport)
-            passport={}
+answera,answerb,i,passport,lines=0,0,0,{},[line.strip() for line in open("../input/day4.txt", "r")]
+while i<=len(lines):
+    if i==len(lines) or len(lines[i]) == 0:
+        if {'byr','iyr','eyr','hgt','hcl','ecl','pid'} <= passport.keys():
+            answera+=1
+            if 1920 <= int(passport.get('byr')) <= 2002 and \
+               2010 <= int(passport.get('iyr')) <= 2020 and \
+               2020 <= int(passport.get('eyr')) <= 2030 and \
+               validheight(passport.get('hgt')) and \
+               bool(re.match("^[#][a-f0-9]{6}$",passport.get('hcl'))) and \
+               passport.get('ecl') in ('amb','blu','brn','gry','grn','hzl','oth') and \
+               bool(re.match("^[0-9]{9}$",passport.get('pid'))):
+                answerb+=1
+        passport={}
     else:
-        for i in l.strip().split(): passport.update({i.split(':')[0]:i.split(':')[1]})
-lst.append(passport)
-for passport in lst:
-    if allrequired(passport):
-        answera+=1
-        if validyear(passport.get('byr'),1920,2002) and \
-           validyear(passport.get('iyr'),2010,2020) and \
-           validyear(passport.get('eyr'),2020,2030) and \
-           validheight(passport.get('hgt')) and \
-           bool(re.match("^[#][a-f0-9]{6}$",passport.get('hcl'))) and \
-           passport.get('ecl') in ('amb','blu','brn','gry','grn','hzl','oth') and \
-           bool(re.match("^[0-9]{9}$",passport.get('pid'))):
-            answerb+=1
+        for j in lines[i].split(): passport.update({j.split(':')[0]:j.split(':')[1]})
+    i+=1
 print (f"{answera} {answerb}")
