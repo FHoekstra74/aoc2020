03f8d0737ad4d27bb9be8fa885013a873d177f61
frans <frans.hoekstra@gmail.com>
2020-12-04 21:27:14 +0100

    because I can, day4 in 11 lines of code, but completely unreadable!

--- python/day4.py
+++ python/day4.py
@@ -1,22 +1,10 @@
 import re
-def validheight(height):
-    if height[-2:] == 'cm': return height[:-2].isnumeric() and int(height[:-2]) >= 150 and int(height[:-2]) <= 193
-    elif height[-2:] == 'in': return height[:-2].isnumeric() and int(height[:-2]) >= 59 and int(height[:-2]) <= 76
-    else: return False
-
 answera,answerb,i,passport,lines=0,0,0,{},[line.strip() for line in open("../input/day4.txt", "r")]
 for i in range(0,len(lines)+1):
     if i == len(lines) or len(lines[i]) == 0:
         if {'byr','iyr','eyr','hgt','hcl','ecl','pid'} <= passport.keys():
             answera += 1
-            if 1920 <= int(passport.get('byr')) <= 2002 and \
-               2010 <= int(passport.get('iyr')) <= 2020 and \
-               2020 <= int(passport.get('eyr')) <= 2030 and \
-               validheight(passport.get('hgt')) and \
-               bool(re.match("^[#][a-f0-9]{6}$",passport.get('hcl'))) and \
-               passport.get('ecl') in ('amb','blu','brn','gry','grn','hzl','oth') and \
-               bool(re.match("^[0-9]{9}$",passport.get('pid'))):
-                answerb += 1
+            if 1920 <= int(passport.get('byr')) <= 2002 and 2010 <= int(passport.get('iyr')) <= 2020 and 2020 <= int(passport.get('eyr')) <= 2030 and ((passport.get('hgt')[-2:] == 'cm' and (150 <= int(passport.get('hgt')[:-2]) <= 193)) or (passport.get('hgt')[-2:] == 'in' and (59 <= int(passport.get('hgt')[:-2]) <= 76))) and bool(re.match("^[#][a-f0-9]{6}$",passport.get('hcl'))) and passport.get('ecl') in ('amb','blu','brn','gry','grn','hzl','oth') and bool(re.match("^[0-9]{9}$",passport.get('pid'))): answerb += 1
         passport={}
     else:
         for j in lines[i].split(): passport.update({j.split(':')[0]:j.split(':')[1]})
