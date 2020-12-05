092d9499db0d9c148d89c23f82ef3df7a550556e
frans <frans.hoekstra@gmail.com>
2020-12-05 08:29:40 +0100

    some cleanup

--- python/day5.py
+++ python/day5.py
@@ -1,16 +1,13 @@
 def calc(boardingpass):
-    rows,cols,rowmin,rowmax,colmin,colmax=128,8,0,127,0,7
+    rowstep,colstep,row,col=128,8,0,0
     for c in boardingpass:
-        if c in ('F','B'): rows = rows // 2
-        if c in ('L','R'): cols = cols // 2
-        if c == 'F': rowmax = rowmax - rows
-        if c == 'B': rowmin = rowmin + rows
-        if c == 'L': colmax = colmax - cols
-        if c == 'R': colmin = colmin + cols
-    return ((rowmax * 8) + colmax)
-seats,f=[],open("../input/day5.txt","r")
-for l in f: seats.append(calc(l.strip()))
+        if c in ('F','B'): rowstep = rowstep // 2
+        if c in ('L','R'): colstep = colstep // 2
+        if c == 'B': row += rowstep
+        if c == 'R': col += colstep
+    return ((row * 8) + col)
+seats=[calc(l.strip()) for l in open("../input/day5.txt","r")]
 print(f"answera: {max(seats)}")
-for seat in range(0,128*8):
+for seat in range(min(seats),max(seats)):
     if seat not in seats and seat+1 in seats and seat-1 in seats:
         print(f"answerb: {seat}")
