8cc44f3f470b6891b106e1f7ccad1515f8972e54
Frans Hoekstra <fransh@schoolmaster.nl>
2020-12-21 11:53:54 +0100

    some cleanup

--- python/day20.py
+++ python/day20.py
@@ -37,9 +37,7 @@ def bottomedge(tile):
     return(tile[-1])
 
 f=open('../input/day20.txt','r')
-tileedges={}
-tiles={}
-tileid=0
+tileedges,tiles,tileid,alledges={},{},0,[]
 for line in f:
     line=line.strip()
     if 'Tile' in line:
@@ -59,12 +57,11 @@ for line in f:
         edges.append(rightedge(tile)[::-1])
         tileedges[tileid]=edges
         tiles[tileid]=tile
-alledges=[]
+
 for edges in tileedges.values():
     for edge in edges: alledges.append(edge)
 
-firstcornertile=0
-answera=1
+firstcornertile,answera=0,1
 for tileid,edges in tileedges.items():
     uniqueedge=0
     for edge in edges:
@@ -75,16 +72,13 @@ for tileid,edges in tileedges.items():
 
 print(answera)
 
-lefttoptile=tiles[firstcornertile]
-lefttopedges=[]
+lefttoptile,lefttopedges=tiles[firstcornertile],[]
 for edge in tileedges[firstcornertile]:
     if alledges.count(edge) == 1: lefttopedges.append(edge)
 
 for i in range(4):
-    if topedge(lefttoptile) in lefttopedges and leftedge(lefttoptile) in lefttopedges:
-        break
-    else:
-        lefttoptile=rotateright(lefttoptile)
+    if topedge(lefttoptile) in lefttopedges and leftedge(lefttoptile) in lefttopedges: break
+    else: lefttoptile=rotateright(lefttoptile)
 
 alltiles={}
 alltiles[(0,0)]=lefttoptile
@@ -94,47 +88,39 @@ for y in range(max):
     for x in range(max):
         if x==0 and y==0: pass
         else:
+            newtileid=0
             if x > 0:
-                newtileid=0
-                if (x-1,y) in alltiles:
-                    edge=rightedge( alltiles[(x-1,y)])
-                    for tileid,edges in tileedges.items():
-                        if edge in edges and tileid not in usedtiles:
-                            newtileid = tileid
-                    newtile=tiles[newtileid]
-                    for i in range(9):
-                        if leftedge(newtile) == edge:
-                            usedtiles.append(newtileid)
-                            alltiles[x,y]=newtile
-                            break
-                        else:
-                            if i==4: newtile=flip(newtile)
-                            else: newtile=rotateright(newtile)
+                edge=rightedge(alltiles[(x-1,y)])
+                for tileid,edges in tileedges.items():
+                    if edge in edges and tileid not in usedtiles:
+                        newtileid = tileid
+                newtile=tiles[newtileid]
+                for i in range(9):
+                    if leftedge(newtile) == edge:
+                        break
+                    else:
+                        if i==4: newtile=flip(newtile)
+                        else: newtile=rotateright(newtile)
             else:
-                newtileid=0
-                if (x,y-1) in alltiles:
-                    edge=bottomedge(alltiles[x,y-1])
-                    for tileid,edges in tileedges.items():
-                        if edge in edges and tileid not in usedtiles:
-                            newtileid = tileid
-                    newtile=tiles[newtileid]
-                    for i in range(9):
-                        if topedge(newtile) == edge:
-                            usedtiles.append(newtileid)
-                            alltiles[x,y]=newtile
-                            break
-                        else:
-                            if i==4: newtile=flip(newtile)
-                            else: newtile=rotateright(newtile)
+                edge=bottomedge(alltiles[x,y-1])
+                for tileid,edges in tileedges.items():
+                    if edge in edges and tileid not in usedtiles:
+                        newtileid = tileid
+                newtile=tiles[newtileid]
+                for i in range(9):
+                    if topedge(newtile) == edge:
+                        break
+                    else:
+                        if i==4: newtile=flip(newtile)
+                        else: newtile=rotateright(newtile)
+            usedtiles.append(newtileid)
+            alltiles[x,y]=newtile
 
-y=0
-n=0
-bigtile=[]
+y,n,bigtile=0,0,[]
 for row in range(max * len(lefttoptile)):
     newline=''
-    for x in range(max):
-        newline+=alltiles[(x,y)][n][1:-1]
-    if n>0 and (n+1)<len(lefttoptile):
+    if 0 < n < len(lefttoptile)-1:
+        for x in range(max): newline+=alltiles[(x,y)][n][1:-1]
         bigtile.append(newline)
     n+=1
     if n==len(lefttoptile):
@@ -145,8 +131,7 @@ monster = []
 monster.append('                  # ')
 monster.append('#    ##    ##    ###')
 monster.append(' #  #  #  #  #  #   ')
-found=False
-f=0
+found,f=False,0
 while not found:
     for i in range(len(bigtile)-len(monster)):
         for j in range(len(bigtile)-len(monster[0])):
@@ -158,22 +143,18 @@ while not found:
             if ok==15:
                 for k in range(len(monster)):
                     for l in range(len(monster[0])):
-                        if monster[k][l] == '#' and bigtile[i+k][j+l] == '#':
+                        if monster[k][l] == '#':
                             oldstr=bigtile[i+k]
                             pos=j+l
                             oldstr = oldstr[:pos] + 'O' + oldstr[pos + 1:]
                             bigtile[i+k]=oldstr
                 found=True
     if not found:
-        if f==4:
-            bigtile=flip(bigtile)
-        else:
-            bigtile=rotateright(bigtile)
+        if f==4: bigtile=flip(bigtile)
+        else: bigtile=rotateright(bigtile)
         f+=1
 #printtile(bigtile)
 
 answerb=0
-for i in range(len(bigtile)):
-    for j in range(len(bigtile)):
-        if bigtile[i][j]=='#': answerb+=1
+for line in bigtile: answerb+=line.count('#')
 print(answerb)
